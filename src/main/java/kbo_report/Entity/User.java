0bb6844f8b4747716a15856fa22f53a75536c043
khi6249 <a01091436249@gmail.com>
2026-04-14 18:42:19 +0900

    login및 회원가입 구현

--- src/main/java/kbo_report/Entity/User.java
+++ src/main/java/kbo_report/Entity/User.java
@@ -18,6 +18,8 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    private String name;
+
     private String email;
     private String favoriteTeam;
 }
--- src/main/java/kbo_report/controller/LoginController.java
+++ src/main/java/kbo_report/controller/LoginController.java
@@ -2,7 +2,6 @@ package kbo_report.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import kbo_report.service.UserService;
 
 @RestController
@@ -12,6 +11,7 @@ public class LoginController {
     @Autowired
     private UserService userService;
 
+    // 1. 로그인 처리
     @PostMapping("/login")
     public String login(@RequestParam String username, @RequestParam String password) {
         boolean isSuccess = userService.login(username, password);
@@ -20,6 +20,27 @@ public class LoginController {
             return "로그인 성공! 환영합니다, " + username + "님.";
         } else {
             return "로그인 실패: 아이디 또는 비밀번호를 확인하세요.";
+        }
+    }
+
+    // 회원가입 처리 (수정된 버전)
+    @PostMapping("/signup")
+    public String signup(@RequestParam String username, 
+                         @RequestParam String password, 
+                         @RequestParam String name) {
+        try {
+            // 1. 새로운 User 엔티티 객체 생성
+            kbo_report.entity.User user = new kbo_report.entity.User();
+            user.setUsername(username);
+            user.setPassword(password);
+            user.setName(name);
+
+            // 2. 서비스의 register 메서드 호출
+            userService.register(user);
+            
+            return "회원가입 성공! 이제 로그인해 주세요.";
+        } catch (Exception e) {
+            return "회원가입 실패: " + e.getMessage();
         }
     }
 }
